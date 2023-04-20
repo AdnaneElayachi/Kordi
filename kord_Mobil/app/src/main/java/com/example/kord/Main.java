f42631c1d3bf483b1af5c7f038e520b3ee5d2e64
Adnane Elayachi <adnaneelayachi1@gmail.com>
2023-04-20 21:36:13 +0000

    KoordsMobil

--- kord_Mobil/app/src/main/java/com/example/kord/Main.java
+++ kord_Mobil/app/src/main/java/com/example/kord/Main.java
@@ -3,12 +3,13 @@ package com.example.kord;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Main extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.dash_board);
     }
 }
---
+++ kord_Mobil/app/src/main/java/com/example/kord/registre_main.java
@@ -0,0 +1,30 @@
+package com.example.kord;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class registre_main extends AppCompatActivity {
+    TextView SignupLink;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registre_activity);
+        SignupLink=findViewById(R.id.SignupLink);
+        SignupLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(registre_main.this,registre_main.class));
+
+            }
+        });
+
+
+    }
+}
